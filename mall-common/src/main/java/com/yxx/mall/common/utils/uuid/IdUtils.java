be9fcfacc4c5703ecb954bd526076aae2b5791f2
xyong <xiaoxuan.yong@morningstar.com>
2021-05-07 15:35:06 +0800

    完成登录验证码生成

---
+++ mall-common/src/main/java/com/yxx/mall/common/utils/uuid/IdUtils.java
@@ -0,0 +1,33 @@
+package com.yxx.mall.common.utils.uuid;
+
+import java.util.UUID;
+
+/**
+ * ID生成器工具类
+ * 
+ * @author ruoyi
+ */
+public class IdUtils
+{
+    /**
+     * 获取随机UUID
+     * 
+     * @return 随机UUID
+     */
+    public static String randomUUID()
+    {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 简化的UUID，去掉了横线
+     * 
+     * @return 简化的UUID，去掉了横线
+     */
+    public static String simpleUUID()
+    {
+        return UUID.randomUUID().toString().replaceAll("-","");
+    }
+
+
+}
