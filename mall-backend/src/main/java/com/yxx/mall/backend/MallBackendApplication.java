56a0a2e3caa3b21b5b1204713c8097bfc2d9a2ad
xyong <xiaoxuan.yong@morningstar.com>
2021-05-07 09:56:48 +0800

    引入若依框架

---
+++ mall-backend/src/main/java/com/yxx/mall/backend/MallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yxx.mall.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallBackendApplication.class, args);
+    }
+
+}
---
+++ mall-backend/src/test/java/com/yxx/mall/backend/MallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxx.mall.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
